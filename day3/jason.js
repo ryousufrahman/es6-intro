ae41464b72af07808b9f880b4ea730d06af7ab4a
yousufrahman <ryousuf766@gmail.com>
2026-03-01 22:40:03 +0600

    day 3 has started with jason

---
+++ day3/jason.js
@@ -0,0 +1,19 @@
+console.log('connected');
+
+ const obj ={
+    name : 'yousuf',
+    age : 21,
+    father : 'ataur',
+    isReach :false
+ }
+ console.log(obj);
+ 
+ const objJason = JSON.stringify(obj)
+ console.log(objJason);
+
+ const objParse =JSON.parse(objJason)
+ console.log(objParse);
+
+//  jason is a javascript object with notation like string
+ 
+ 
