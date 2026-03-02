6556c7c28b5f9b4db6a9b229fdf588e10e3ba6ce
yousufrahman <ryousuf766@gmail.com>
2026-03-02 14:34:22 +0600

    showed the json data card in page with style

--- day3/loadPost.js
+++ day3/loadPost.js
@@ -3,14 +3,26 @@ const loadpost =()=>{
     .then(allpost=> allpost.json())
     .then(data =>display(data) )
 }
- const ulcontainer =document.getElementById('ul-contianer')
+ 
+       
 const display =(posts)=>{
+    const postcontainer =document.getElementById('post-container')
+     postcontainer.innerHTML=''
     posts.forEach(element => {
+      
+        const div =document.createElement('div')
+        div.innerHTML= `
+         <div class="post">
+            <h2>${element.title}</h2>
+            <p>${element.body}</p>
+
+        </div>
         
-        const li =document.createElement('li')
-        li.innerText = element.body
-        ulcontainer.appendChild(li)
+        `
+        postcontainer.appendChild(div);
+        
         
     });
 
 }
+loadpost();
